4d90825315dbdff44056ca9872a43be7ea6e171a
JuliaCz18 <julkacz@onet.pl>
2024-03-17 17:58:25 +0100

    pomiar_temp i status_led

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,60 @@
 #include <Arduino.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+
+// Pin config (wybranie pinu na esp32 do podlaczenia)
+const int oneWireBus = 4; 
+const int errorPin = 25; 
+const int correctPin = 26;
+
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(oneWireBus);
+
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+
+float pomiar_temperatury()
+{
+  sensors.requestTemperatures(); 
+  float temperatureC = sensors.getTempCByIndex(0);
+  return temperatureC;
+}
+
+void blad()
+{
+  digitalWrite(correctPin, LOW);
+  digitalWrite(errorPin, HIGH);
+}
+
+void dziala()
+{
+  digitalWrite(errorPin, LOW);
+  digitalWrite(correctPin, HIGH);
+}
 
 void setup()
-{}
+{
+  // Start the Serial Monitor
+  Serial.begin(9600);
+  // Start the DS18B20 sensor
+  sensors.begin();
+  //setup errorPin as output
+  pinMode(errorPin, OUTPUT);
+  //setup correctPin as output
+  pinMode(correctPin, OUTPUT);
+}
 
 void loop()
-{}
+{
+  Serial.println("Zmierzona temperatura: ");
+  Serial.print(pomiar_temperatury());
+  Serial.println("ºC");
+  delay(1000);
+  //diody LED - monitoring
+  blad();
+  delay(1000);
+  dziala();
+  delay(1000);
+}
 
